04587335cdde36fc1ae010f6f398ccbb5f5e8c93
Duke <dukekhaos@gmail.com>
2015-04-09 02:49:10 -0300

    update layout

---
+++ www/assets/app.js
@@ -0,0 +1,17 @@
+var COLORS = [
+  {bg: "#FFC107", text: "black"},
+  {bg: "#2196F3", text: "white"},
+  {bg: "#E91E63", text: "white"},
+  {bg: "#F44336", text: "white"},
+  {bg: "#4CAF50", text: "white"},
+  {bg: "#FF5722", text: "white"}
+];
+
+window.addEventListener('polymer-ready', function(e) {
+  var toolbar = document.querySelector('core-toolbar');
+  var color = COLORS[Math.floor(Math.random()*COLORS.length)];
+  toolbar.style.backgroundColor = color.bg;
+  toolbar.style.color = color.text;
+
+  debugger
+});
--- www/index.js
+++ www/index.js
@@ -2861,21 +2861,4 @@
   ;
 
     Polymer('movie-list',{});
-  ;
-
-        var colors = [
-          {bg: "#FFC107", text: "black"},
-          {bg: "#2196F3", text: "white"},
-          {bg: "#E91E63", text: "white"},
-          {bg: "#F44336", text: "white"},
-          {bg: "#4CAF50", text: "white"},
-          {bg: "#FF5722", text: "white"}
-        ];
-
-        window.addEventListener('polymer-ready', function(e) {
-          var toolbar = document.querySelector('core-toolbar');
-          var color = colors[Math.floor(Math.random()*colors.length)];
-          toolbar.style.backgroundColor = color.bg;
-          toolbar.style.color = color.text;
-        });
-      
+  
